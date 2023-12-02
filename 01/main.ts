0a1ccbf643937020a583c0dcb36b70e21e7b93ef
Matt Darveniza <matt.darveniza@kaleris.com>
2023-12-02 12:51:01 +1100

    day one part two

--- 01/main.ts
+++ 01/main.ts
@@ -1,17 +1,51 @@
 import { readline } from "https://deno.land/x/readline@v1.1.0/mod.ts";
 
-const f = await Deno.open("./calibration.txt");
+const numberMap: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+const regex = new RegExp(
+  `(?=([0-9]|${Object.keys(numberMap).join("|")}))`,
+  "g"
+);
 
-let sum = 0;
-const decoder = new TextDecoder();
-for await (const uIntLine of readline(f)) {
-  const lineChars = [...decoder.decode(uIntLine)];
-  const first = lineChars.find((c) => /[0-9]/.test(c));
-  const last = lineChars.findLast((c) => /[0-9]/.test(c));
+function match(str: string) {
+  const regexMatch = [...str.matchAll(regex)];
 
-  const num = parseInt(`${first}${last}`, 10);
-  sum += num;
+  if (regexMatch.length === 0) {
+    throw new Error("No match found");
+  }
+
+  return [0, -1].map((index) => {
+    const value = regexMatch.at(index)?.[1] as string;
+
+    if (value in numberMap) {
+      return numberMap[value];
+    }
+
+    return parseInt(value, 10);
+  });
 }
 
-console.log(sum);
-f.close();
+export async function sumNumbers(filePath: string) {
+  const f = await Deno.open(filePath);
+
+  let sum = 0;
+  const decoder = new TextDecoder();
+  for await (const uIntLine of readline(f)) {
+    const line = decoder.decode(uIntLine);
+    const [first, last] = match(line);
+
+    const num = parseInt(`${first}${last}`, 10);
+    sum += num;
+  }
+
+  return sum;
+}
---
+++ main.ts
@@ -0,0 +1,3 @@
+import { sumNumbers } from "./01/main.ts";
+
+console.log(await sumNumbers("01/calibration.txt"));
