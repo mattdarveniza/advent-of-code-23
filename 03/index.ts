9a58c8f775ec45bf512a2fc0e9a252f7fec3706a
Matt Darveniza <matt.darveniza@kaleris.com>
2023-12-03 22:11:07 +1100

    day 3 part 2

--- 03/index.ts
+++ 03/index.ts
@@ -26,7 +26,7 @@ export async function enginePartSum(filePath: string) {
       // get start and end indices. Covers number plus one to left and right
       // for adjacencies.
       const startIndex = Math.max((partNumberMatch.index ?? 0) - 1, 0);
-      const endIndex = startIndex + partNumber.length + 2; // +2 = 1 to reach end of number, 1 for adjacency
+      const endIndex = startIndex + partNumber.length + 2; // +2 = 1 to cancel -1 above, 1 for adjacency
 
       // check for symbols on same, above and below lines
       const match = [i - 1, i, i + 1].some((index) => {
@@ -41,4 +41,47 @@ export async function enginePartSum(filePath: string) {
   }
 
   return sum;
+}
+
+export async function gearRatios(filePath: string) {
+  const lines: string[] = await readFile(filePath);
+  let ratiosSum = 0;
+
+  for (const [i, line] of lines.entries()) {
+    const gears = [...line.matchAll(/\*/g)];
+
+    for (const gear of gears) {
+      const ratios: number[] = [];
+      const gearStartIndex = Math.max((gear.index ?? 0) - 1, 0);
+      const gearEndIndex = gearStartIndex + 2;
+
+      // look for numbers in the adjacent lines which intersect with the range
+      // of indices
+      [i - 1, i, i + 1].forEach((index) => {
+        const numbers = [...lines[index].matchAll(/\d+/g)];
+        for (const numberMatch of numbers) {
+          const [number] = numberMatch;
+          const numberStartIndex = numberMatch.index ?? 0;
+          const numberEndIndex = numberStartIndex + number.length - 1;
+
+          if (
+            numberEndIndex >= gearStartIndex &&
+            numberStartIndex <= gearEndIndex
+          ) {
+            ratios.push(parseInt(number, 10));
+          }
+        }
+      });
+
+      if (ratios.length === 2) {
+        ratiosSum += ratios[0] * ratios[1];
+      }
+
+      if (ratios.length > 2) {
+        throw new Error("Too many ratios");
+      }
+    }
+  }
+
+  return ratiosSum;
 }
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 import { sumNumbers } from "./01/main.ts";
 import { validGames, powerOfGames } from "./02/index.ts";
-import { enginePartSum } from "./03/index.ts";
+import { enginePartSum, gearRatios } from "./03/index.ts";
 
-console.log(await enginePartSum("03/partNumbers.txt"));
+console.log(await gearRatios("03/partNumbers.txt"));
