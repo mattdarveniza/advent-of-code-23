395710c5b08547b8c517e973cf6e1b8e94eef567
Matt Darveniza <matt.darveniza@kaleris.com>
2023-12-06 13:38:46 +1100

    day 5: working implementation that will take 26 hours to process 😭"

--- 05/index.ts
+++ 05/index.ts
@@ -2,14 +2,7 @@ import { readFile } from "../readFile.ts";
 
 type SeedMap = [number, number, number];
 
-export async function lowestLocation(filePath: string) {
-  const lines: string[] = await readFile(filePath);
-
-  const seeds = lines[0]
-    .split(" ")
-    .slice(1)
-    .map((v) => parseInt(v, 10));
-
+function parseLinesToSeedMap(lines: string[]): SeedMap[][] {
   const maps: SeedMap[][] = [];
   for (let i = 2; i < lines.length; i++) {
     let line = lines[i];
@@ -22,17 +15,89 @@ export async function lowestLocation(filePath: string) {
     }
   }
 
-  const locations = maps.reduce((acc, map) => {
-    return acc.map((inputValue) => {
+  return maps;
+}
+
+function* seedsToLocations(
+  seeds: Iterable<number>,
+  maps: SeedMap[][]
+): Generator<number> {
+  for (const inputValue of seeds) {
+    yield maps.reduce((input, map) => {
       for (const [destination, source, range] of map) {
-        if (inputValue >= source && inputValue <= source + range) {
-          return inputValue + (destination - source);
+        if (input >= source && input <= source + range) {
+          return input + (destination - source);
         }
       }
+      return input;
+    }, inputValue);
+  }
+}
 
-      return inputValue;
-    });
-  }, seeds);
+export async function lowestLocationFromSeedNumbers(filePath: string) {
+  const lines: string[] = await readFile(filePath);
 
+  const seeds = lines[0]
+    .split(" ")
+    .slice(1)
+    .map((v) => parseInt(v, 10));
+
+  const maps = parseLinesToSeedMap(lines);
+  const locations = seedsToLocations(seeds, maps);
   return Math.min(...locations);
+}
+
+function* seedsFromRanges(ranges: [number, number][]) {
+  for (const [start, end] of ranges) {
+    for (let i = start; i <= end; i++) {
+      yield i;
+    }
+  }
+}
+
+export async function lowestLocationFromSeedRanges(filePath: string) {
+  const lines: string[] = await readFile(filePath);
+
+  const seedRanges = lines[0]
+    .split(" ")
+    .slice(1)
+    .reduce((acc: [number, number][], _, i, array) => {
+      if (i % 2 === 0) {
+        const [start, range] = array
+          .slice(i, i + 2)
+          .map((v) => parseInt(v, 10));
+        acc.push([start, start + range]);
+      }
+      return acc;
+    }, []);
+
+  const seeds = seedsFromRanges(seedRanges);
+
+  const seedCount = seedRanges.reduce((acc, [, end]) => acc + end, 0);
+
+  console.log(`Total values: ${seedCount}`);
+
+  const maps = parseLinesToSeedMap(lines);
+  const locations = seedsToLocations(seeds, maps);
+
+  let min = Infinity;
+  let i = 0;
+  let progress = 0;
+  for (const location of locations) {
+    const newProgress = Math.floor((i++ / seedCount) * 100);
+    console.log(`${i} / ${seedCount} ${newProgress}%: ${min}`);
+    if (newProgress > progress) {
+      progress = newProgress;
+      console.log(
+        `\n----------------- Progress: ${progress}% -----------------\n`
+      );
+    }
+
+    if (location < min) {
+      min = location;
+    }
+  }
+
+  console.log("\n------------ Complete ------------\n");
+  return min;
 }
--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ import { sumNumbers } from "./01/main.ts";
 import { validGames, powerOfGames } from "./02/index.ts";
 import { enginePartSum, gearRatios } from "./03/index.ts";
 import { winningCombinations, totalCards } from "./04/index.ts";
-import { lowestLocation } from "./05/index.ts";
+import {
+  lowestLocationFromSeedNumbers,
+  lowestLocationFromSeedRanges,
+} from "./05/index.ts";
 
-console.log(await lowestLocation("05/input.txt"));
+console.log(await lowestLocationFromSeedRanges("05/input.txt"));
